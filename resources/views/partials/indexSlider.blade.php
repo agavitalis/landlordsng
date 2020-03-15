14bb81bc0740e3f683e673a253cf33dae690dad9
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-03-15 14:26:05 +0100

    Fixing of some responsive stuff

--- resources/views/partials/indexSlider.blade.php
+++ resources/views/partials/indexSlider.blade.php
@@ -2,20 +2,20 @@
   <div id="rev_overlaped" class="rev_slider"  data-version="5.0">
     <ul>
       <!-- SLIDE -->
-      <li data-transition="fade">
+      <!-- <li data-transition="fade">
         <img src="images/home1-banner1.jpg" alt="" data-bgposition="center center" data-bgfit="cover" class="rev-slidebg">							
       </li>
       <li data-transition="fade">
         <img src="images/home1-banner2.jpg" alt="" data-bgposition="center center" data-bgfit="cover" class="rev-slidebg">							
-      </li>
+      </li> -->
       <li data-transition="fade">
         <img src="images/home1-banner3.jpg" alt="" data-bgposition="center center" data-bgfit="cover" class="rev-slidebg">							
       </li>
       <div class="tp-static-layers">
-        <div class="tp-caption tp-static-layer" 
+        <div class="tp-caption tp-static-layerz" 
           id="slide-37-layer-2" 
-          data-x="['left','left','left','left']" data-hoffset="['50','50','50','50']" 
-          data-y="['bottom','bottom','bottom','bottom']" data-voffset="['230','180','150','100']"  
+          data-x="['left']" data-hoffset="['50']" 
+          data-y="['bottom','bottom']" data-voffset="['230','500']"  
           data-whitespace="nowrap"
           data-visibility="['on','on','on','on']"
           data-fontsize="['48','48','28','28']"
@@ -24,35 +24,35 @@
           data-basealign="slide" 
           data-startslide="0" 
           data-endslide="5" 
-          style="z-index: 5;">
-          <h1><span class="t_white">We Can Find just Right <br>Property for You.</span></h1>
+          style="z-index:5;padding:1em">
+          <h1><span class="t_white">We can find the right <br>property for you.</span></h1>
         </div>
         <div class="tp-caption tp-static-layer" 
           id="slide-37-layer-2" 
-          data-x="['left','left','left','left']" data-hoffset="['50','50','50','50']" 
-          data-y="['bottom','bottom','bottom','bottom']" data-voffset="['150','100','120','120']" 
+          data-x="['left']" data-hoffset="['50']" 
+          data-y="['bottom','bottom']" data-voffset="['150',300]" 
           data-whitespace="nowrap"
           data-visibility="['on','on','on','on']"
           data-start="500" 
           data-basealign="slide" 
           data-startslide="0" 
           data-endslide="5" 
-          style="z-index: 5;">
-          <p class="t_white">We Deal with Different kinds of Properties No matter you need a House, 
-            an Apartment or garage. <br> You’ll find a good option on our Theme.
+          style="z-index:5;padding:1em">
+          <p class="t_white">We Deal with different kinds of properties.<br> Do you need a Land, House, 
+            an Apartment or a garage. <br> You’ll find a good option on our Website.
           </p>
         </div>
         <div class="tp-caption tp-static-layer" 
           id="slide-37-layer-2" 
-          data-x="['left','left','left','left']" data-hoffset="['50','50','50','50']" 
-          data-y="['bottom','bottom','bottom','bottom']" data-voffset="['60','60','60','60']" 
+          data-x="['left']" data-hoffset="['50']" 
+          data-y="['bottom']" data-voffset="['60']" 
           data-whitespace="nowrap"
           data-visibility="['on','on','on','on']"
           data-start="500" 
           data-basealign="slide" 
           data-startslide="0" 
           data-endslide="5" 
-          style="z-index: 5;"><a href="listing1.html" class="btn-white border_radius uppercase">view Properties</a>
+          style="z-index: 5;padding:1em"><a href="listing1.html" class="btn-white border_radius uppercase">view Properties</a>
         </div>
       </div>
     </ul>
