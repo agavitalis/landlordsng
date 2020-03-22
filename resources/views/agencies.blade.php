803f4ae1a017c3db943e7ef89a8e6e63ec8490b6
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-03-22 02:25:24 +0100

    broken codes

--- resources/views/agencies.blade.php
+++ resources/views/agencies.blade.php
@@ -11,10 +11,10 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <h1 class="text-uppercase">Agencies</h1>
-                <p>Serving you since 1999. Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                <p>Trusted Agencies in LandlordNG.</p>
                 <ol class="breadcrumb text-center ">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Pages</a></li>
+                    <li><a href="/">Home</a></li>
+                   
                     <li><a href="#">Agencies</a></li>
 
                 </ol>
@@ -28,30 +28,31 @@
 <section id="our-agent" class="padding_bottom_half padding_top bg_light">
     <div class="container">
         <div class="row">
-            <div class="ouragents clearfix">
+            @foreach($agencies as $agency)
+
+            
+            <div class="col-md-6 ouragents clearfix">
                 <div class="col-md-3 col-sm-6 heading_space">
-                    <div class="image"><img src="images/agent-three1.jpg" alt="Owl Image"></div>
+                    <div class="image"><img src="storage/uploads/{{$agency->profile_picture}}" alt="Owl Image"></div>
                 </div>
                 <div class="col-md-3 col-sm-6 text-center  bg_white heading_space">
-                    <h3>Bohdan Kononets</h3>
-                    <span>sales executive</span>
-                    <p class="top20 bottom20">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-                        nibh.</p>
-                    <a class="btn-more" href="agent_profile.html"><i><img alt="arrow" src="images/arrow-yellow.png"></i>
-                        <span>Full Profile</span><i><img alt="arrow" src="images/arrow-yellow.png"></i></a>
+                    <h3>{{$agency->agency_name}}</h3>
+                    <span>{{$agency->email}}</span>
+                    <p class="top20 bottom20">
+                        {{$agency->biography}}
+                    </p>
+                    <a class="btn-more" href="/agency_profile"><i><img alt="arrow" src="images/arrow-yellow.png"></i>
+                        <span>Full Profile</span><i><img alt="arrow" src="images/arrow-yellow.png"></i>
+                    </a>
+                    <br>
+                    <a class="btn-more" href="/become_an_agent"><i><img alt="arrow" src="images/arrow-yellow.png"></i>
+                        <span>Register as an Agent</span><i><img alt="arrow" src="images/arrow-yellow.png"></i>
+                    </a>
                 </div>
-                <div class="col-md-3 col-sm-6 heading_space">
-                    <div class="image"><img src="images/agent-three2.jpg" alt="Owl Image"></div>
-                </div>
-                <div class="col-md-3 col-sm-6 text-center  bg_white heading_space">
-                    <h3>Bohdan Kononets</h3>
-                    <span>sales executive</span>
-                    <p class="top20 bottom20">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-                        nibh.</p>
-                    <a class="btn-more" href="agent_profile.html"><i><img alt="arrow" src="images/arrow-yellow.png"></i>
-                        <span>Full Profile</span><i><img alt="arrow" src="images/arrow-yellow.png"></i></a>
-                </div>
+                
             </div>
+            @endforeach
+
             <div class="ouragents clearfix">
                 <div class="col-md-3 col-sm-6 heading_space">
                     <div class="image"><img src="images/agent-three3.jpg" alt="Owl Image"></div>
