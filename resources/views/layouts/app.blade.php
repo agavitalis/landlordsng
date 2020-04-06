9a2ec16812817da1a59be836da9a299eb1965bee
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-04-06 23:47:59 +0100

    over hauled the project for phase two

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,19 +8,19 @@
  <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <title>{{ config('app.name', 'Laravel') }}</title>
-<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="../css/reality-icon.css">
-<link rel="stylesheet" type="text/css" href="../css/bootsnav.css">
-<link rel="stylesheet" type="text/css" href="../css/jquery.fancybox.css">
-<link rel="stylesheet" type="text/css" href="../css/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="../css/owl.transitions.css">
-<link rel="stylesheet" type="text/css" href="../css/cubeportfolio.min.css">
-<link rel="stylesheet" type="text/css" href="../css/settings.css">
-<link rel="stylesheet" type="text/css" href="../css/range-Slider.min.css">
-<link rel="stylesheet" type="text/css" href="../css/search.css">
+<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/reality-icon.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/bootsnav.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/jquery.fancybox.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/owl.carousel.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/owl.transitions.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/cubeportfolio.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/settings.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/range-Slider.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/search.css')}}">
 @yield('styles')
-<link rel="stylesheet" type="text/css" href="../css/style.css">
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 <link rel="icon" href="images/icon.png">
 </head>
 <body>
@@ -57,29 +57,29 @@
 <!--Footer-->
 @yield('footer')
 <!--Footer Ends-->
-<script src="../js/jquery-2.1.4.js"></script> 
-<script src="../js/bootstrap.min.js"></script> 
-<script src="../js/jquery.appear.js"></script>
-<script src="../js/jquery-countTo.js"></script>
-<script src="../js/bootsnav.js"></script>
-<script src="../js/masonry.pkgd.min.js"></script>
-<script src="../js/jquery.parallax-1.1.3.js"></script>
-<script src="../js/jquery.cubeportfolio.min.js"></script>
-<script src="../js/range-Slider.min.js"></script>
-<script src="../js/owl.carousel.min.js"></script> 
-<script src="../js/selectbox-0.2.min.js"></script>
-<script src="../js/zelect.js"></script>
-<script src="../js/jquery.fancybox.js"></script>
-<script src="../js/jquery.themepunch.tools.min.js"></script>
-<script src="../js/jquery.themepunch.revolution.min.js"></script>
-<script src="../js/revolution.extension.actions.min.js"></script>
-<script src="../js/revolution.extension.layeranimation.min.js"></script>
-<script src="../js/revolution.extension.navigation.min.js"></script>
-<script src="../js/revolution.extension.parallax.min.js"></script>
-<script src="../js/revolution.extension.slideanims.min.js"></script>
-<script src="../js/revolution.extension.video.min.js"></script>
-<script src="../js/custom.js"></script>
-<script src="../js/functions.js"></script>
+<script src="{{asset('js/jquery-2.1.4.js')}}"></script> 
+<script src="{{asset('js/bootstrap.min.js')}}"></script> 
+<script src="{{asset('js/jquery.appear.js')}}"></script>
+<script src="{{asset('js/jquery-countTo.js')}}"></script>
+<script src="{{asset('js/bootsnav.js')}}"></script>
+<script src="{{asset('js/masonry.pkgd.min.js')}}"></script>
+<script src="{{asset('js/jquery.parallax-1.1.3.js')}}"></script>
+<script src="{{asset('js/jquery.cubeportfolio.min.js')}}"></script>
+<script src="{{asset('js/range-Slider.min.js')}}"></script>
+<script src="{{asset('js/owl.carousel.min.js')}}"></script> 
+<script src="{{asset('js/selectbox-0.2.min.js')}}"></script>
+<script src="{{asset('js/zelect.js')}}"></script>
+<script src="{{asset('js/jquery.fancybox.js')}}"></script>
+<script src="{{asset('js/jquery.themepunch.tools.min.js')}}"></script>
+<script src="{{asset('js/jquery.themepunch.revolution.min.js')}}"></script>
+<script src="{{asset('js/revolution.extension.actions.min.js')}}"></script>
+<script src="{{asset('js/revolution.extension.layeranimation.min.js')}}"></script>
+<script src="{{asset('js/revolution.extension.navigation.min.js')}}"></script>
+<script src="{{asset('js/revolution.extension.parallax.min.js')}}"></script>
+<script src="{{asset('js/revolution.extension.slideanims.min.js')}}"></script>
+<script src="{{asset('js/revolution.extension.video.min.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+<script src="{{asset('js/functions.js')}}"></script>
 @yield('scripts')
 </body>
 </html>
